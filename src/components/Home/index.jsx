ac4aec9ba2a7dc1015acd00377c7860071a7fe8c
Rogelio Pacheco Elorza <rogelio_133@outlook.com>
2021-03-24 22:27:10 -0600

    se agrega imagen principal en home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,6 +4,6 @@ export const Home = () => (
   <>
     <h1 className='title is-1'>Bienvenido a Cuestionarios</h1>
     <h4 className='title is-4'>El sitio donde puedes crear cuestionarios y compartirlos con tus amigos</h4>
-    <hr />
+    <img style={{ width: 550, margin: '0 auto', display: 'block' }} alt='screenshot' width={550} src='https://raw.githubusercontent.com/rogelio133/cuestionarios-frontend/main/screenshots/06.png' />
   </>
 )
